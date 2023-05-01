3b1a0ec34a8d7bd7b9d44fc2e6dac89d8b6e7a51
André Faria <andre.faria@weclever.co>
2023-04-30 22:22:47 -0300

    remove log

--- reviewIndiviualTemplate.js
+++ reviewIndiviualTemplate.js
@@ -5,5 +5,4 @@ const client = require("twilio")(accountSid, authToken);
 client.content.v1
   .contents("")
   .fetch()
-  //.then(content => console.log(content.friendlyName));
   .then((content) => console.log(content));
